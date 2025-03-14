37b59777589664acae16c2a2231dcefe5aeeb6e6
aideisayevaa <aideisayeva3@gmail.com>
2025-03-14 22:59:52 +0400

    update

--- effect.js
+++ effect.js
@@ -151,12 +151,12 @@ $('document').ready(function () {
 		$('#b5').attr('id', 'b55')
 		$('#b6').attr('id', 'b66')
 		$('#b7').attr('id', 'b77')
-		$('#b11').animate({ top: 240, left: vw - 200 }, 500);
-		$('#b22').animate({ top: 240, left: vw - 120 }, 500);
-		$('#b33').animate({ top: 240, left: vw - 40 }, 500);
-		$('#b44').animate({ top: 240, left: vw + 40 }, 500);
-		$('#b55').animate({ top: 240, left: vw + 120 }, 500);
-		$('#b66').animate({ top: 240, left: vw + 200 }, 500);
+		$('#b11').animate({ top: 240, left: vw - 150 }, 500);
+		$('#b22').animate({ top: 240, left: vw - 90 }, 500);
+		$('#b33').animate({ top: 240, left: vw - 30 }, 500);
+		$('#b44').animate({ top: 240, left: vw + 30 }, 500);
+		$('#b55').animate({ top: 240, left: vw + 90 }, 500);
+		$('#b66').animate({ top: 240, left: vw + 150 }, 500);
 		$('.balloons').css('opacity', '0.9');
 		$('.balloons h2').fadeIn(3000);
 		$(this).fadeOut('slow').delay(3000).promise().done(function () {
